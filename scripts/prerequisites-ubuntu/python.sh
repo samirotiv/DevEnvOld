04cc7ebbb2826e3c5d8e8f302fa7d094d7b7200b
samirotiv <samirotiv@gmail.com>
2017-08-30 18:37:59 +0000

    Update to virtualenv

--- scripts/prerequisites-ubuntu/python.sh
+++ scripts/prerequisites-ubuntu/python.sh
@@ -1,5 +1,6 @@
 apt install python-pip python3-pip -y
 apt install python-dev python3-dev -y
+apt install python3-venv -y
 pip2 install --upgrade pip setuptools virtualenv
-pip3 install --upgrade pip setuptools virtualenv
+pip3 install --upgrade pip setuptools
 
