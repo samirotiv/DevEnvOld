56454f0af0a6d3cc4a3a283db95c13a65a2b0378
samirotiv <samirotiv@gmail.com>
2017-08-30 18:33:44 +0000

    Updated python

--- scripts/prerequisites-ubuntu/python.sh
+++ scripts/prerequisites-ubuntu/python.sh
@@ -1,2 +1,5 @@
 apt install python-pip python3-pip -y
-apt-get install python-dev python3-dev -y
+apt install python-dev python3-dev -y
+pip2 install --upgrade pip setuptools virtualenv
+pip3 install --upgrade pip setuptools virtualenv
+
