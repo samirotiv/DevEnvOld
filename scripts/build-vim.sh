3fdc07fd1661a2b4a1d6b2f83c46c98eda25afa0
samirotiv <samirotiv@gmail.com>
2017-08-30 18:12:41 +0000

    Updated build scripts

--- scripts/build-vim.sh
+++ scripts/build-vim.sh
@@ -4,4 +4,4 @@ export LC_ALL=C
 
 cd ../packages/vim
 sudo apt-get install libncurses5-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
-./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-python3interp=yes --with-python3-config-dir=/usr/lib/python3.5/config --enable-perlinterp=yes --enable-luainterp=yes --enable-cscope --prefix=/usr
+./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-python3interp=yes --with-python3-config-dir=/usr/lib/python3.5/config --enable-perlinterp=yes --enable-luainterp=yes --enable-cscope --prefix=/usr/local
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,2 +1,7 @@
+cd "${0%/*}"
+cur=$PWD
+export LC_ALL=C
+cd ..
+
 cp -R home/. ~/
 cat ~/dev_exports >> ~/.bashrc
