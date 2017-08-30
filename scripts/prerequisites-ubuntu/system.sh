99fc8e510432ddcb0808fa5e5663962cc6e310c0
samirotiv <samirotiv@gmail.com>
2017-08-30 10:23:53 +0000

    Update to prereqs

--- scripts/prerequisites-ubuntu/system.sh
+++ scripts/prerequisites-ubuntu/system.sh
@@ -1,4 +1,5 @@
 sudo apt-get install software-properties-common -y
+sudo apt-get install openssh-server -y
 sudo apt-get install tmux bmon silversearcher-ag -y
 
 sudo apt-get install build-essential cmake -y
